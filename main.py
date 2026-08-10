76246cd0b7c056066c2e551601a93918e6c72701
Claudson Luciano <lclaudson2020@gmail.com>
2026-08-10 12:20:07 -0300

    Primeiro commit do sistema de cadastro

---
+++ main.py
@@ -0,0 +1,10 @@
+print("=== Sistema de Cadastro ===")
+
+nome = input("Nome: ")
+idade = input("Idade: ")
+email = input("Email: ")
+
+print("\n=== Dados Cadastrados ===")
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"email: {email}")
